94a93ba6e3bce2ba651288867a552245a9eea80c
Jordan Muniz <jordy.valois@gmail.com>
2025-01-09 22:16:01 -0500

    Added Support For Pose Detection
    - Added requirements for pose detection, and have a thumbs up detector working

---
+++ Assets/ChangeColor.cs
@@ -0,0 +1,45 @@
+using UnityEngine;
+
+public class ChangeColor : MonoBehaviour
+{
+    // Reference to the object's Renderer
+    private Renderer objectRenderer;
+
+    // Public boolean to toggle between red and blue
+    public bool toggle = false;
+    public bool isRed = false; 
+
+    void Start()
+    {
+        // Get the Renderer component attached to the object
+        objectRenderer = GetComponent<Renderer>();
+
+        // Ensure the object has a material to modify
+        if (objectRenderer == null || objectRenderer.material == null)
+        {
+            Debug.LogError("No Renderer or Material found on this object!");
+        }
+    }
+
+    void Update()
+    {
+        if(toggle)
+        {
+            toggle = false;
+            ToggleColor();
+        }
+    }
+
+    // Function to toggle the material color
+    public void ToggleColor()
+    {
+        if (objectRenderer != null && objectRenderer.material != null)
+        {
+            objectRenderer.enabled = false;
+        }
+        else
+        {
+            Debug.LogError("Cannot change color: No Renderer or Material available!");
+        }
+    }
+}
