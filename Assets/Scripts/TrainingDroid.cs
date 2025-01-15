f30c815dd99d7847503e633bfd7be50fd075117b
Oliver Wacker <oliverwacker22@gmail.com>
2025-01-14 19:14:17 -0500

    Future Vision + random error fix

--- Assets/Scripts/TrainingDroid.cs
+++ Assets/Scripts/TrainingDroid.cs
@@ -8,73 +8,116 @@ using Oculus.Interaction;
 using System.Collections.Generic;
 using System;
 
-public class TrainingDroid: MonoBehaviour {
-    public GameObject head;
-     public GameObject eye;
-    public GameObject body;
+public class TrainingDroid : MonoBehaviour
+{
+	public GameObject head;
+	public GameObject eye;
+	public GameObject body;
 	public float bodyRadius;
 	public GameObject player;
-	public Transform pointA;
 	public Transform pointB;
 	public float speed;
-	private Rigidbody rb;
-	private bool reachedA;
-	private float angularVelocity;
-	private float headZ;
 	public Transform headProxy;
 	private List<Vector3> futurePositions = new List<Vector3>();
+	public float worldWidth;
 
+	public GameObject ghost;
+	public GameObject ghostBody;
+	public GameObject ghostHead;
+	public Transform ghostHeadProxy;
+
+	public bool inFuture;
+	public int futureIndex;
+	public int futureSteps;
+	//public PostProcess shader;
 
 	private Vector3 spinDirection;
 	void Start()
 	{
-
-		headZ = head.transform.rotation.z;
-		rb = GetComponent<Rigidbody>();
 		// Set initial position of the ball at the start point
 		spinDirection = pointB.position - transform.position;
-		for (int i = 0; i < 15; i++) {
+		for (int i = 0; i < 15; i++)
+		{
 			addFuture(true);
 		}
-		
+		ghost.SetActive(false);
+		//shader.enabled = false;
+
 	}
 
 	void Update()
-    {
-		headZ = head.transform.rotation.z;
-		
+	{
 
-		Vector3 target = new Vector3(head.transform.position.x,head.transform.position.y, player.transform.position.z);
 		headProxy.LookAt(player.transform);
-		if (body.transform.position == pointB.position)
+		ghostHeadProxy.LookAt(player.transform);
+		if ((body.transform.position == pointB.position) ||( inFuture &&(ghost.transform.position == pointB.position)))
 		{
-			pointB.position = futurePositions[0];
-			addFuture();
-			spinDirection = (pointB.position-transform.position);
-			Debug.Log(spinDirection.magnitude);
-			spinDirection.x /= spinDirection.magnitude;
-			spinDirection.y /= spinDirection.magnitude;
-			spinDirection.z /= spinDirection.magnitude;
+			reachedPoint();
+
 		}
-		
-		var step = Time.deltaTime*speed;
-		Vector3 direction = Vector3.MoveTowards(body.transform.position, pointB.position, step);
-		transform.position = direction;
+
+		var step = Time.deltaTime * speed;
 		float degX = ((spinDirection.x * step)) / bodyRadius * Mathf.Rad2Deg;
 		float degZ = ((spinDirection.z * step)) / bodyRadius * Mathf.Rad2Deg;
 		float degY = ((spinDirection.y * step)) / bodyRadius * Mathf.Rad2Deg;
-		body.transform.Rotate(degX, degY, degZ);
+		if (!inFuture)
+		{
+			Vector3 direction = Vector3.MoveTowards(body.transform.position, pointB.position, step);
+			transform.position = direction;
+			body.transform.Rotate(degX, degY, degZ);
+		}
+		Vector3 ghostDirection = Vector3.MoveTowards(ghostBody.transform.position, pointB.position, step);
+		ghost.transform.position = ghostDirection;
+		ghostBody.transform.Rotate(degX, degY, degZ);
 
 
 	}
 	void addFuture(bool init = false)
 	{
-		Vector3 pos = new Vector3(Random.Range(-3.5f, 3.5f), 0.5f, Random.Range(-3.5f, 3.5f));
+		float upper = worldWidth / 2f;
+		float lower = -1 * upper;
+		Vector3 pos = new Vector3(UnityEngine.Random.Range(lower, upper), 0.5f, UnityEngine.Random.Range(lower, upper));
 		futurePositions.Add(pos);
-		if(!init)
+		if (!init)
 		{
 			futurePositions.RemoveAt(0);
 		}
+	}
+
+	public void seeFuture()
+	{
+		inFuture = true;
+		ghost.SetActive(true);
+		futureIndex = 0;
+		//shader.enabled =true;
+	}
+	void reachedPoint()
+	{
+		
+		pointB.position = futurePositions[futureIndex];
+		Debug.Log(inFuture.ToString());
+		if (inFuture)
+		{
+			Debug.LogWarning( futureIndex.ToString());
+			futureIndex++;
+			if ((futureIndex == futureSteps))
+			{
+				inFuture = false;
+				//shader.enabled = false;
+				futureIndex = 0;
+				ghost.SetActive(false);
+				pointB.position = futurePositions[0];
+			}
+		}
+		else
+		{
+			addFuture();
+			
+		}
+		spinDirection = (pointB.position - transform.position);
+		spinDirection.x /= spinDirection.magnitude;
+		spinDirection.y /= spinDirection.magnitude;
+		spinDirection.z /= spinDirection.magnitude;
 	}
 
 
