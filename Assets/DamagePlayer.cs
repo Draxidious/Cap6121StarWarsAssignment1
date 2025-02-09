bb25d4644b66725487e3b12cb0ee930bdc09ea72
Jordan Muniz <jordy.valois@gmail.com>
2025-02-09 15:27:06 -0500

    Implemented Player Damage

---
+++ Assets/DamagePlayer.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+
+public class DamagePlayer : MonoBehaviour
+{
+    public Player player;
+    public AudioSource oof;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("LaserInitial"))
+        {
+            player.health -= 5;
+            oof.Play();
+        }
+    }
+}
